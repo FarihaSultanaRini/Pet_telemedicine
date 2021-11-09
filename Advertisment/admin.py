8ff39e740cf60ce39be2a28e30e28cc423535b4d
Rini <70469277+FarihaSultanaRini@users.noreply.github.com>
2021-11-09 11:50:55 +0600

    Delete Advertisment directory

--- Advertisment/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .advertisment_models import Advertisement
-
-# Register your models here.
-admin.site.register(Advertisement)
--- Advertisment/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .advertisment_models import Advertisement
-
-class Insertadvertisment(forms.ModelForm):
-    class Meta :
-        model = Advertisement
-        fields = ( 'pet', 'image1', 'image2')
